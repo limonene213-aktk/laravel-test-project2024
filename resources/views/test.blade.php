651a464a112ce69619f06ab9d12f29621ab52d7d
limonene213u <katana_400s@outlook.jp>
2024-11-22 12:13:39 +0900

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,47 @@
+
+    @if (Route::has('login'))
+        @auth
+            <a
+                href="{{ url('/dashboard') }}"
+                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+            >
+                ダッシュボード
+            </a>
+        @else
+            @guest
+                <p>
+                    ログインしていません。
+                </p>
+            @endguest
+            <a
+                href="{{ route('login') }}"
+                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+            >
+                ログイン
+            </a>
+            <div>
+            @if (Route::has('register'))
+                <a
+                    href="{{ route('register') }}"
+                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                >
+                  登録
+                </a>
+            @endif
+        @endauth
+    @endif
+
+
+<div>
+<p>こんにちは</p>
+<div>
+@auth
+    <p>
+        {{Auth::user()->name}} さん、こんにちは。
+    </p>
+@endauth
+
+<script>
+    //alert("この祠を壊したのは誰じゃ！？")
+</script>
+
