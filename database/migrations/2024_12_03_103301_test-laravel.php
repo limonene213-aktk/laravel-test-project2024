77cd90f55baca35b667a55d98c70c78cabcb887a
limonene213u <katana_400s@outlook.jp>
2024-12-03 10:51:04 +0900

    commit-2024_12_3

--- database/migrations/2024_12_03_103301_test-laravel.php
+++ database/migrations/2024_12_03_103301_test-laravel.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('tests', function(Blueprint $table){
             $table->id();
+            $table->string('fruits')->unique();
+            $table->string('favorite');
             $table->timestamps();
         });
     }
