0a27497d70a8755518a6940e65dd3f68b6ed311b
limonene213u <katana_400s@outlook.jp>
2024-12-10 22:20:14 +0900

    fix controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,3 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function create() {
+        return view('post.create');
+    }
+}
+
